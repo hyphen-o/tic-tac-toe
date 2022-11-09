0c28c86da0b33c49ad5d93effd005c7461815f37
keigo-0314 <s256053@wakayama-u.ac.jp>
2022-11-09 19:00:05 +0900

    Complete project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import Board from './Board';
 import './index.css'
+
+class Game extends React.Component{
+  render() {
+    return (
+        <div className="game">
+          <Board></Board>
+        </div>
+    )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game></Game>);
